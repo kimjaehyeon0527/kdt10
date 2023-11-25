05b5c45de7884f3effee687164b6de4b999ef619
kimjaehyeon0527 <kimdh66665@naver.com>
2023-11-25 09:24:54 +0900

    form

--- 231121/05_promise/promise.js
+++ 231121/05_promise/promise.js
@@ -71,39 +71,39 @@
 // ------------ 프로미스 체이닝 ------------
 // 함수를 이용해서 ( 4 + 3 ) * 2 - 1 = 13 을 연산하기!
 
-// // 1. 콜백함수라면?
-// function add(n1, n2, cb) {
-//     setTimeout(function() {
-//         const result = n1 + n2;
-//         cb(result);     // mul(result)
-//     }, 1000);
-// }
+// 1. 콜백함수라면?
+function add(n1, n2, cb) {
+    setTimeout(function() {
+        const result = n1 + n2;
+        cb(result);     // mul(result)
+    }, 1000);
+}
 
-// function mul(n, cb) {
-//     setTimeout(function() {
-//         const result = n * 2;
-//         cb(result); // sub(result)
-//     }, 700);
-// }
+function mul(n, cb) {
+    setTimeout(function() {
+        const result = n * 2;
+        cb(result); // sub(result)
+    }, 700);
+}
 
-// function sub(n, cb) {
-//     setTimeout(function() {
-//         const result = n - 1;
-//         cb(result);
-//     }, 500);
-// }
+function sub(n, cb) {
+    setTimeout(function() {
+        const result = n - 1;
+        cb(result);
+    }, 500);
+}
 
-// add(4, 3, function(result1) {
-//     console.log('add result :', result1);
+add(4, 3, function(result1) {
+    console.log('add result :', result1);
 
-//     mul(result1, function(result2) {
-//         console.log('mul result :', result2);
+    mul(result1, function(result2) {
+        console.log('mul result :', result2);
 
-//         sub(result2, function(result3){
-//             console.log('sub result :', result3);
-//         });
-//     })
-// })
+        sub(result2, function(result3){
+            console.log('sub result :', result3);
+        });
+    })
+})
 
 // 2. 프로미스 체이닝이라면?
 function addPromise(n1, n2) {
---
+++ 231121/06_form/실습.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const 실습 = express();
+const PORT = 8001;
+
+실습.set('view engine', 'ejs');
+// 실습.set('/views', 'views');
+실습.set('views', './views');
+
+
+실습.use(express.urlencoded({extended: true})); // post 요청으로 들어오는 모든 형식의 데이터를 파싱
+실습.use(express.json()); // json 형식으로 데이터를 주고 받음.
+
+실습.get('/', (req, res) => {
+    res.render('실습');
+})
+
+실습.get('/test', (req, res) => {
+    res.render('./test/test');
+})
+
+실습.get('/quest', (req, res) => {
+    res.render('result2', { questInfo : req.query})
+    // res.render('result2', {req.query})
+})
+
+실습.post('/quest', (req, res) => {
+    res.render('result2', { title: 'Post 실습' , questInfo : req.body})
+})
+
+
+실습.listen(PORT, function() {
+    console.log(`${PORT} is opening!`);
+})
+
+// Q1. 앞부분 실습. 관련  ---> express의 객체를 뜻하는 거라 맘대로 바꿔도 상관이없다 . 통일만 해주면 될 뿐
+// Q2. post 사용할때 필수인가 ---> 필수이다.
