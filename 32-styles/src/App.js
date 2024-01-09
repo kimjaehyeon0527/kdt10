011a9e66a9fbcbb225614049d3829d0cccf8e7c1
kimjaehyeon0527 <kimdh66665@naver.com>
2024-01-09 13:55:57 +0900

    React router

--- 32-styles/src/App.js
+++ 32-styles/src/App.js
@@ -1,8 +1,10 @@
 import SassComponent from './SassComponent';
 import CssModuleComponent from './CssModuleComponent';
 import './styles/App.css';
+import PostList from './PostList';
+import StyledComponent from './StyledComponent';
 // import Practice1 from './Practice1';
-import Practice2 from './Practice2';
+// import Practice2 from './Practice2';
 
 function App() {
   return (
@@ -20,7 +22,13 @@ function App() {
       {/* <Practice1 /> */}
       <hr />
 
-      <Practice2 />
+      {/* <Practice2 /> */}
+      <hr />
+      <PostList />
+      <hr />
+
+      <h2>Styled-Components</h2>
+      <StyledComponent />
     </div>
   );
 }
